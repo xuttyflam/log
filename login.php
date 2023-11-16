68db7c2020f9a60da8bfe14bced4f0bfa110b331
xuttyflam <146911474+xuttyflam@users.noreply.github.com>
2023-11-16 03:41:51 -1200

    Add files via upload

---
+++ login.php
@@ -0,0 +1,35 @@
+<?php 
+session_start(); 
+include "db.php";
+
+if (isset($_POST['uname']) && isset($_POST['password'])) {
+
+	function validate($data){
+       $data = trim($data);
+	   $data = stripslashes($data);
+	   $data = htmlspecialchars($data);
+	   return $data;
+	}
+
+	$uname = validate($_POST['uname']);
+	$pass = validate($_POST['password']);
+
+	if (empty($uname)) {
+		header("Location: index.php?error=User Name is required");
+	    exit();
+	}else if(empty($pass)){
+        header("Location: index.php?error=Password is required");
+	    exit();
+	}else{
+		$sql = "SELECT * FROM users WHERE user_name='$uname' AND password='$pass'";
+
+		$result = mysqli_query($conn, $sql);
+        echo "hello, still lacking skills in php but :D";
+
+		
+	}
+	
+}else{
+	header("Location: index.php");
+	exit();
+}
